dda5ec035c8435464816e5500c907f2e5221a050
DESKTOP-71IBSPO\utente <montano_corsojava@tecninf.it>
2020-12-17 16:59:39 +0100

    modal + chiamataPost

--- js/threatnews.js
+++ js/threatnews.js
@@ -19,7 +19,8 @@ function loadPulses() {
         success: function(data) {
             $(document).ready(function() {
                 $("[id='pulsename']").click(function() {
-                    alert($(this).attr("titlemodal"));
+                    $("#titleId").html($(this).attr("titleModal"));
+                    $("#descriptionId").html($(this).attr("descriptionModal"));
                 });
             });
             HTMLpulse(data);
@@ -85,30 +86,32 @@ function HTMLpulse(data) {
 
         }
     })
-    HTMLModal();
+    htmlModal();
 }
 
 function htmlNames() {
     $("#mostraNomi")
-        .append("<div class='col-md-3'; id ='pulsename';titlemodal='ciaociao';>" + "<div id = 'active-bar'> <i class ='fas fa-atom';style='text-align:center'></i></div>" +
+        .append(
+            "<div class='col-md-3' id ='pulsename' descriptionModal='" + descriptionJSON.replace(/['"]+/g, '') +
+            "'titleModal='" + contentJSON.replace(/['"]+/g, '') + "'style='cursor:pointer' data-toggle='modal' data-target='#exampleModal'>" +
+            "<div id = 'active-bar' style='padding-top:auto important!;padding-bottom:auto important!'> <i class ='fas fa-atom'></i></div>" +
             "<div style='float:left'>" + "<img src='./img/avatar.png'></div>" +
             "<div style='float:right;margin:10px;'>" +
-            "<a href  data-toggle='modal' data-target='#exampleModal'> " +
+            "<div  > " +
             contentJSON.replace(/['"]+/g, '') +
-            "</a>" +
+            "</div>" +
             "</div></div>");
 
 }
 
 
 
-function HTMLModal() {
-    $("#exampleModalLabel").append("<p>" + contentJSON.replace(/['"]+/g, '') + "</p>");
-    $("#mb").append("<p>" + descriptionJSON.replace(/['"]+/g, '') + "</p>");
+function htmlModal() {
+    $("#exampleModalLabel").append("<p id='titleId' style='text-align:center'>" + contentJSON.replace(/['"]+/g, '') + "</p>");
+    $("#mb").append("<p id='descriptionId' style='text-align:justify'>" + descriptionJSON.replace(/['"]+/g, '') + "</p>");
+
 
 }
-
-
 
 //non in uso
 function pulseInsert(name, description) {
@@ -127,74 +130,74 @@ function pulseInsert(name, description) {
             alert("success");
         }
     });
+}
 
-    //
+//
 
-    function buttonInsertPulse(data) {
+function buttonInsertPulse(data) {
 
-        $.each(data, function(key, firstValue) {
-            if (key == "results") {
-                var contenutoJSON = JSON.stringify(firstValue);
-                console.log(JSON.stringify(key));
-                $.each(firstValue, function(secondKey, secondValue) {
-                    console.log(JSON.stringify(secondValue.name))
-                    contentJSON = JSON.stringify(secondValue.name);
-                    descriptionJSON = JSON.stringify(secondValue.description);
-                    this.pulseInsert(contentJSON, descriptionJSON);
-
-
-                })
-
-            }
-        })
-        $("#btnInvia").click(buttonInsertPulse);
-    }
+    $.each(data, function(key, firstValue) {
+        if (key == "results") {
+            var contenutoJSON = JSON.stringify(firstValue);
+            console.log(JSON.stringify(key));
+            $.each(firstValue, function(secondKey, secondValue) {
+                console.log(JSON.stringify(secondValue.name))
+                contentJSON = JSON.stringify(secondValue.name);
+                descriptionJSON = JSON.stringify(secondValue.description);
+                this.pulseInsert(contentJSON, descriptionJSON);
 
 
+            })
+
+        }
+    })
+    $("#btnInvia").click(buttonInsertPulse);
+}
 
 
-    // Our labels along the x-axis
-    $(document).ready(function() {
-        console.log("s3r_chart");
-        var data = {
-                labels: ['Trojan',
-                    'Backdoor',
-                    'Worm',
-                    'Bitcoin Miner',
-                    'File Infector',
-                    'Other'
-                ],
-                datasets: [{
-                    data: [1200, 1700, 800, 200, 999, 424],
-                    backgroundColor: [
-                        "rgba(255, 0, 0, 0.5)",
-                        "rgba(100, 255, 0, 0.5)",
-                        "rgba(200, 50, 255, 0.5)",
-                        "rgba(10, 110, 255, 0.5)",
-                        "rgba(110, 120, 255, 0.5)",
-                        "rgba(210, 130, 255, 0.5)"
-                    ]
-                }],
 
-                // These labels appear in the legend and in the tooltips when hovering different arcs
-                labels: [
-                    'Trojan',
-                    'Backdoor',
-                    'Worm',
-                    'Bitcoin Miner',
-                    'File Infector',
-                    'Other'
+
+// Our labels along the x-axis
+$(document).ready(function() {
+    console.log("s3r_chart");
+    var data = {
+            labels: ['Trojan',
+                'Backdoor',
+                'Worm',
+                'Bitcoin Miner',
+                'File Infector',
+                'Other'
+            ],
+            datasets: [{
+                data: [1200, 1700, 800, 200, 999, 424],
+                backgroundColor: [
+                    "rgba(255, 0, 0, 0.5)",
+                    "rgba(100, 255, 0, 0.5)",
+                    "rgba(200, 50, 255, 0.5)",
+                    "rgba(10, 110, 255, 0.5)",
+                    "rgba(110, 120, 255, 0.5)",
+                    "rgba(210, 130, 255, 0.5)"
                 ]
-            }
-            //                label_def ="01-01-2011","01-01-2014,01-01-2013,01-01-2012"
-        var ctx = document.getElementById('threat_news_chart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            data: data,
-            type: 'polarArea',
-            //        options: options
+            }],
 
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Trojan',
+                'Backdoor',
+                'Worm',
+                'Bitcoin Miner',
+                'File Infector',
+                'Other'
+            ]
+        }
+        //                label_def ="01-01-2011","01-01-2014,01-01-2013,01-01-2012"
+    var ctx = document.getElementById('threat_news_chart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        data: data,
+        type: 'polarArea',
+        //        options: options
 
-        });
 
     });
-}
+
+});
